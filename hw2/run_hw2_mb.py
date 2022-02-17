8e0098d7a5703d623122065ca537e4c5bcaf0d6d
jaihon1 <jaihon.qasim@hotmail.com>
2022-02-17 16:23:57 -0500

    adding runs for question4.

--- hw2/run_hw2_mb.py
+++ hw2/run_hw2_mb.py
@@ -141,9 +141,9 @@ if __name__ == "__main__":
 # Question 4: Commands
 # python run_hw2_mb.py exp_name=q4_reacher_horizon5 env_name=reacher-ift6163-v0 add_sl_noise=true mpc_horizon=5 num_agent_train_steps_per_iter=1000 batch_size=800 n_iter=5 video_log_freq=-1 mpc_action_sampling_strategy='random'
 
-# python run_hw2_mb.py exp_name=q4_reacher_horizon15 env_name=reacher-ift6163-v0 add_sl_noise=true mpc_horizon=15 num_agent_train_steps_per_iter=1000 batch_size=800 n_iter=5 video_log_freq=-1 mpc_action_sampling_strategy='random'
+# python run_hw2_mb.py exp_name=q4_reacher_horizon15 env_name=reacher-ift6163-v0 add_sl_noise=true mpc_horizon=15 num_agent_train_steps_per_iter=1000 batch_size=800 n_iter=1 video_log_freq=-1 mpc_action_sampling_strategy='random'
 
-# python run_hw2_mb.py exp_name=q4_reacher_horizon30 env_name=reacher-ift6163-v0 add_sl_noise=true mpc_horizon=30 num_agent_train_steps_per_iter=1000 batch_size=800 n_iter=5 video_log_freq=-1 mpc_action_sampling_strategy='random'
+# python run_hw2_mb.py exp_name=q4_reacher_horizon30 env_name=reacher-ift6163-v0 add_sl_noise=true mpc_horizon=30 num_agent_train_steps_per_iter=1000 batch_size=800 n_iter=1 video_log_freq=-1 mpc_action_sampling_strategy='random'
 
 # python run_hw2_mb.py exp_name=q4_reacher_numseq100 env_name=reacher-ift6163-v0 add_sl_noise=true mpc_horizon=10 num_agent_train_steps_per_iter=1000 batch_size=800 n_iter=1 mpc_num_action_sequences=100 mpc_action_sampling_strategy='random'
 
@@ -154,3 +154,8 @@ if __name__ == "__main__":
 # python run_hw2_mb.py exp_name=q4_reacher_ensemble3 env_name=reacher-ift6163-v0 ensemble_size=3 add_sl_noise=true mpc_horizon=10 num_agent_train_steps_per_iter=1000 batch_size=800 n_iter=1 video_log_freq=-1 mpc_action_sampling_strategy='random'
 
 # python run_hw2_mb.py exp_name=q4_reacher_ensemble5 env_name=reacher-ift6163-v0 ensemble_size=5 add_sl_noise=true mpc_horizon=10 num_agent_train_steps_per_iter=1000 batch_size=800 n_iter=1 video_log_freq=-1 mpc_action_sampling_strategy='random'
+
+
+# no multivariate model
+# diagonal covariance
+# np.random.normal(0, 1, (1,))
